5c80d0f17c70d71338f3349e9d6175f38515584b
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-05-29 11:11:49 +0100

    Another commit

--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -2,13 +2,14 @@ import React from 'react'
 import tekstor from '../asset/tekstor.jpg'
 import amazon from '../asset/amazon-clone.png'
 import clock from '../asset/digital-clock.png'
+import portfolio from "../asset/portfolio.png"
 
 function RecentWork() {
   return (
     <div className='w-full bg-white py-16 px-4' id='recentWorks'>
             <p className='md:ml-32 ml-5 font-Kaushan font-bold text-left md:text-3xl sm:text-2xl text-1xl mb-4'>Recent Works</p>
        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 md:grid-rows-2'>
-            <div className='px-3 border-solid border-2 border-light-blue-500 rounded-2xl mr-2 mb-2'>
+            <div className='px-3 rounded-2xl mr-2 mb-2'>
             <img src={tekstor}alt="" className='mb-4' />
             <h1 className='font-[Poppins]'>TEKSTOR ECOMMERCE</h1>
             </div>
@@ -16,13 +17,13 @@ function RecentWork() {
             <img src={amazon}alt="" className='mb-4 h-[19.5em] mx-auto pt-3' />
             <h1 className='font-[Poppins]'>AMAZON CLONE</h1>
             </div>
-            <div className='px-3 border-solid border-2 border-light-blue-500 rounded-2xl mb-2'>
+            <div className='px-3 rounded-2xl mb-2'>
             <img src={clock}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/>
             <h1 className='font-[Poppins]'>DIGITAL CLOCK</h1>
             </div>
-            <div className='px-3 border-solid border-2 border-light-blue-500 rounded-2xl'>
-            <img src={tekstor}alt="" className='mb-4' />
-            <h1 className='font-[Poppins]'>TEKSTOR ECOMMERCE</h1>
+            <div className='px-3 rounded-2xl mb-2'>
+            <img src={portfolio}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/>
+            <h1 className='font-[Poppins]'>PORTFOLIO WEBSITE</h1>
             </div>
             
 
