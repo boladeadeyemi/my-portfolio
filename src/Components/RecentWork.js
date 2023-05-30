af9d5fb438c62094a2a13c631aefbb944e35304c
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-05-30 15:09:17 +0100

    new commit

--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -10,20 +10,20 @@ function RecentWork() {
             <p className='md:ml-32 ml-5 font-Kaushan font-bold text-left md:text-3xl sm:text-2xl text-1xl mb-4'>Recent Works</p>
        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 md:grid-rows-2'>
             <div className='px-3 rounded-2xl mr-2 mb-2'>
-            <img src={tekstor}alt="" className='mb-4' />
-            <h1 className='font-[Poppins]'>TEKSTOR ECOMMERCE</h1>
+            <a href="https://tekstor-code-nest.vercel.app/"  target='_blank'><img src={tekstor}alt="" className='mb-4' /></a>
+            <h1 className='font-Poppins'>TEKSTOR ECOMMERCE</h1>
             </div>
             <div className='px-3 rounded-2xl mr-2 mb-2'>
-            <img src={amazon}alt="" className='mb-4 h-[19.5em] mx-auto pt-3' />
-            <h1 className='font-[Poppins]'>AMAZON CLONE</h1>
+            <a href="https://tekstor-code-nest.vercel.app/" target='_blank'><img src={amazon}alt="" className='mb-4 h-[19.5em] mx-auto pt-3' /></a>
+            <h1 className='font-Poppins'>AMAZON CLONE</h1>
             </div>
             <div className='px-3 rounded-2xl mb-2'>
-            <img src={clock}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/>
-            <h1 className='font-[Poppins]'>DIGITAL CLOCK</h1>
+            <a href="https://bolade-digital-clock.vercel.app/" target='_blank'><img src={clock}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/></a>
+            <h1 className='font-Poppins'>DIGITAL CLOCK</h1>
             </div>
             <div className='px-3 rounded-2xl mb-2'>
-            <img src={portfolio}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/>
-            <h1 className='font-[Poppins]'>PORTFOLIO WEBSITE</h1>
+            <a href="https://my-portfolio-zg1z.vercel.app/" target='_blank'><img src={portfolio}alt="" className='mb-4 h-[19.5em] mx-auto pt-3'/></a>
+            <h1 className='font-Poppins'>PORTFOLIO WEBSITE</h1>
             </div>
             
 
