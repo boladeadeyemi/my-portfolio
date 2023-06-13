f76c952a8186c38a0fe3dda8a377af8b2f0e00fb
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-06-13 14:26:46 +0100

    font changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-      backgroundImage: {},
-      fontFamily: {
-        Kaushan: ['Kaushan Script', 'cursive'],
-        Poppins:['Poppins', 'sans-serif']
-      }
-
+    backgroundImage: {},
+    fontFamily: {
+      Kaushan: ["Kaushan Script", "cursive"],
+      Poppins: ["Poppins", "sans-serif"],
+    },
   },
   variants: {},
-  plugins: [
-  ],
- 
-}
-
+  plugins: [],
+};
