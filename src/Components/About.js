048dadd43826a19c3c821623e0faab1a7cfb0886
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-06-14 21:07:25 +0100

    added more features

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,19 +20,22 @@ function About() {
   return (
     <div className="w-full bg-white py-16 px-4 overflow-x-hidden" id="about">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img className="w-80 mx-auto my-4" src={Bolade} alt="bolade" />
+        <img
+          className="w-80 mx-auto my-4 rounded-xl drop-shadow-2xl"
+          src={Bolade}
+          alt="bolade"
+        />
         <div className="flex flex-col justify-center mx-auto h-96">
           <h1 className="font-Kaushan md:text-4xl sm:text-3xl text-2xl text-black font-bold mb-6 md:mx-0 px-2 text-justify">
             I am Bolade Adeyemi, <br></br>
             Front-End Developer <br />
-            From Lagos State, Nigeria.
           </h1>
           <p className=" md:text-lg sm:text-sm text-[0.8em]  px-2 mb-6 font-Poppins text-[#999999] mx-auto">
             I have rich experience in web site design & building and
-            customization. Also I am good at html, css, javascript, wordpress,
-            php, jquery, bootstrap. I love to talk with you about our unique
-            approach. Feel free to contact me writing an email with your project
-            idea.
+            customization. Also I am good at html, css, javascript, typescript,
+            bootstrap, tailwind css, and reactjs. I love to talk with you about
+            our unique approach. Feel free to contact me writing an email with
+            your project idea.
           </p>
           <button
             className="py-2 text-center bg-[#49bc56] w-[40%] md:text-sm sm:text-[0.9em] text-[0.7em] text-white font-Poppins ml-2"
--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -2,7 +2,6 @@ import React from "react";
 import tekstor from "../asset/tekstor.jpg";
 import amazon from "../asset/amazon-clone.png";
 import clock from "../asset/digital-clock.png";
-import portfolio from "../asset/portfolio.png";
 import youtube from "../asset/youtube-clone.png";
 import gym from "../asset/gym-exercise.png";
 
@@ -69,20 +68,7 @@ function RecentWork() {
           </a>
           <h1 className="font-Poppins">DIGITAL CLOCK</h1>
         </div>
-        <div className="px-3 rounded-2xl mb-2">
-          <a
-            href="https://my-portfolio-zg1z.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src={portfolio}
-              alt=""
-              className="mb-4 h-[20.4em] mx-auto pt-3 mt-[-19px] transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-90 ..."
-            />
-          </a>
-          <h1 className="font-Poppins">PORTFOLIO WEBSITE</h1>
-        </div>
+
         <div className="px-3 rounded-2xl mr-2 mb-2">
           <a
             href="https://amazonclone-delta.vercel.app/"
